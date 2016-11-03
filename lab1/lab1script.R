843701361bb4a7c0416fbef0b65c6feca0b81adf
Sebastian Maghsoudi <sebma654@student.liu.se>
2016-11-03 12:06:13 +0100

    Added for asssignement 2 part 1 and 2

---
+++ lab1/lab1script.R
@@ -0,0 +1,24 @@
+# Not used, ignore it.
+prop = function(x,theta){
+  return(theta * exp(-theta*x))
+}
+
+# Log-likelihood function.
+loglike = function(theta,vec){  
+    return(length(vec)*log(theta) - theta*sum(vec))  
+}
+
+# Fatal amount of values.
+seqstep = seq(0.05,4,0.0001)
+
+# Result of log-liklelohjdiofsyh given fatal many attempts with different theta.
+loglikevec = loglike(seqstep,machines[,1])
+
+# Draws a nice looking graph.
+plot(seqstep,loglikevec, col="red",type = "l", xlab="theta", ylab="likelihood for theta")
+
+# According to the plot the value of theta that yields highest likelihood.
+maxlike = seqstep[which.max(loglikevec)]
+
+# Not used, saved for later.
+# estimatedlam = length(machines[,1])/(sum(machines))
