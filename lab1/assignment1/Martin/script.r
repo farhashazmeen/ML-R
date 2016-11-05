fc124d4cb94a3806272d664100222aa4a120a7c1
Martin Estgren <martin.estgren@gmail.com>
2016-11-05 10:29:41 +0100

    Fixed sens and spec functions

--- lab1/assignment1/Martin/script.r
+++ lab1/assignment1/Martin/script.r
@@ -30,13 +30,13 @@ knearest <- function(train, K, test) {
   nospam_train = train[,-ncol(train)]
   nospam_test = test[, -ncol(test)]
   
-  X_h = train/ sqrt(rowSums(nospam_train^2))
-  Y_h = test/ sqrt(rowSums(nospam_test^2))
+  X_h = train/sqrt(rowSums(nospam_train^2))
+  Y_h = test/sqrt(rowSums(nospam_test^2))
   C = X_h %*% t(Y_h)
-  distance = 1.0 - C
-  k_distance = t(apply(distance,1,order)[,1:K])
+  distance = 1 - C
+  k_distance = t(apply(distance,1,order))
   spam = apply(k_distance,1,spam,lookup_table=train)
-  spam_means = rowMeans(spam)
+  spam_means = rowMeans(as.matrix(spam[,1:K]))
   return (spam_means)
   
 }
@@ -48,25 +48,27 @@ threshold <- function(threshold, data){
 
 #Function to calcuate the sensitivity of a data set
 sensitivity <- function(observations, predictions){
-  result = sum((observations == 1 & predictions == observations))/ sum(predictions == 1) 
+  result = sum((observations == 1 & predictions == 1))/ (sum((observations == 1 & predictions == 1)) + sum(observations == 1 & predictions == 0)) 
   return(result);
 }
 
 #Function to calcuate the specificity of a data set
 specificity <- function(observations, predictions){
-  result = sum((observations == 0 & predictions == observations))/ sum(predictions == 0) 
-  return(result);
+  result = sum((observations == 0 & predictions == 0))/ (sum((observations == 0 & predictions == 0)) + sum(observations == 0 & predictions == 1))
+  return(result)
 }
 
 observations = test$Spam
 knearest_k5 = as.numeric(knearest(as.matrix(train),5,as.matrix(test)) > 0.5)
 knearest_k1 = as.numeric(knearest(as.matrix(train),1,as.matrix(test)) > 0.5)
 
-print(table(observations,knearest_k5))
-print(table(observations,knearest_k1))
+mst_k5 = table(observations,knearest_k5)
+mst_k1 = table(observations,knearest_k1)
+print(mst_k5)
+print(mst_k1)
 
-print(mean(knearest_k5 != observations))
-print(mean(knearest_k1 != observations))
+print(1 - sum(diag(mst_k5) / sum(mst_k5)))
+print(1 - sum(diag(mst_k1) / sum(mst_k1)))
 
 # Check with build-in functions
 kknn_predictions_k5 = kknn(formula = Spam ~ ., train = train, test = test ,k = 5)
@@ -83,11 +85,8 @@ print(mean(kknn_predictions_k1 != observations))
 
 # Check with other prediction thresholds
 
-# Specify the threshold range
-thresholds = seq(from = 0.05, to = 0.95, by = 0.05)
-
 # Converts to matrix for apply operations
-thresholds = matrix(thresholds,length(thresholds),1)
+thresholds = matrix(seq(from = 0.05, to = 0.95, by = 0.05),length(thresholds),1)
 
 # Get the knearest predictions of a data set
 knearest_predictions = knearest(as.matrix(train),5,as.matrix(test))
@@ -109,4 +108,6 @@ kknn_specificity = apply(kknn_outcomes,1,specificity, observations = observation
 
 # Plot the values as ROC curves
 plot(1-knearest_specificity,knearest_sensitivity, xlim = c(0,1), ylim = c(0,1), xlab = "FPR", ylab = "TPR")
-points(1-kknn_specificity,kknn_sensitivity, col="Green")
+lines(1-knearest_specificity,knearest_sensitivity, col="Blue")
+lines(1-kknn_specificity,kknn_sensitivity, col="Green")
+lines(c(0,1),c(0,1), col="Red")
