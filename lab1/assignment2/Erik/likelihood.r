32c3b1db5df8df32f5ac4730530f95dd3e207790
Erik S. V. Jansson <caffeineviking@gmail.com>
2016-11-06 23:59:30 +0100

    Almost complete assignment 2.

--- lab1/assignment2/Erik/likelihood.r
+++ lab1/assignment2/Erik/likelihood.r
@@ -8,3 +8,9 @@ lnlikelihood <- function(x, theta) {
     p <- log(distribution(x, theta))
     return(sum(p)) # log-likelihood.
 }
+
+polikelihood <- function(x, theta) {
+    jp<-prod(distribution(x, theta))
+    posterior <- 10*exp(-10*theta)
+    return(log(jp*posterior))
+}
