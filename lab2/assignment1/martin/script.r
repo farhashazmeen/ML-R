a6301cb6153d0c21f810ef28c34970ab20aa5fbf
sci10n <martin.estgren@gmail.com>
2016-11-12 00:20:27 +0100

    worked on lab 2  a 1, now just to figure out SSE

--- lab2/assignment1/martin/script.r
+++ lab2/assignment1/martin/script.r
@@ -3,78 +3,80 @@ library(readxl)
 # folding indexes, returns start indexes for each fold
 indexes <- function(n,k){
   s = floor(n/k)
-  indexes = matrix(0,k,2)
+  indexes = matrix(1,k,2)
   for(i in 1:k){
     indexes[i,1] = (i-1) * s
     indexes[i,2] = i * s -1
   }
+  indexes = indexes +1
   indexes[k,2] = n
   return(indexes)
 }
 
 binary_permutations <- function(n){
-  indexes <- t(sapply(1:(n^2-2),function(x){as.integer(intToBits(x))})[1:n,])
+  indexes = matrix(0,2^n-1,n)
+  for(i in 1:2^n-1){
+    indexes[i,] = as.numeric(intToBits(i))[1:n]
+  }
+  return(indexes)
 }
 
 best_subset <- function(X,Y,K){
   n = nrow(X)
+  
+  dummy_features = binary_permutations(ncol(X))
+  feature_combinations = nrow(dummy_features)
   indexes = indexes(n,K)
-  for( i in 1:K){
-    test_indexes = indexes[i,1]:indexes[i,2]
-    test_fold = X[test_indexes,]
-    training_fold = X[-test_indexes,]
-    dummy_features = binary_permutations(ncol(X))
-    errors = matrix(0,K,1)
-    prediction = 
+  
+  errors = matrix(0,feature_combinations,1)
+  predictions = matrix(0,feature_combinations,n)
+  for(combination in 1:feature_combinations){
+    current_features = which(dummy_features[combination,] == 1)
+    filtered_x = as.matrix(X[current_features])
+
+    mean_fold_errors = matrix(0,K,1)
+    for( i in 1:K){
+      # 
+      test_fold_indexes = indexes[i,1]:indexes[i,2]
+      train_fold_indexes = (1:n)[-test_fold_indexes]
+    
+      test_y = Y[test_fold_indexes]
+      test_x = filtered_x[test_fold_indexes,]
+      train_x = filtered_x[train_fold_indexes,]
+      train_y = Y[train_fold_indexes]
+      result = linear_regression(as.matrix(train_x), train_y,as.matrix(test_x),test_y)
+      mean_fold_errors[i,] = result$err
+     # predictions[combination,] = result$pred
+    }
+    errors[combination,] = mean(mean_fold_errors)
+ 
   }
+  print(errors)
+  best_features = which(dummy_features[which.min(errors),] == 1)
+  return(best_features)
 }
 
-# k_folds <- function(X,Y,K) {
-#     col_x = ncol(X)
-#     errors = matrix(0,K,1)
-#     dummy_folds = matrix(0,K,col_x)
-# 
-#     # For every fold
-#     for(i in 1:K){
-#       # Pick random number of columns as sample
-#       n = sample(1:col_x,1)
-#       fold_indexes = sample(1:col_x,n)
-#       # Dumyfy the columns
-#       dummy_folds[i,fold_indexes] = 1
-#       # Get X-values for current fold
-#       fold_x = as.matrix(X[,fold_indexes])
-#       res = linear_regression(fold_x,Y)
-#       errors[i,] = res$err
-#       intercept = res$param[1,]
-#       plot(rowMeans(X),res$pred)
-#       slope = mean(res$param[2:nrow(res$param),])
-#       # points(1:nrow(data),res$pred, col="Green")
-#       abline(intercept,slope)
-#     }
-#   best = which.min(errors)
-#   print(best)
-#   print(dummy_folds)
-#   return(which(dummy_folds[best,] == 1))
-# 
  # Linear regression between two samples, one as x-values and one as y-values
- linear_regression <- function(X,Y){
+ linear_regression <- function(x_train,y_train,x_test,y_test){
    # Add column with ones in X
-   hx = matrix(1,nrow(X), ncol(X)+1)
-   hx[,2:ncol(hx)] = X
-   X = hx 
+  x_train = cbind(matrix(1,nrow(x_train),1),x_train)
+  x_test = cbind(matrix(1,nrow(x_test),1),x_test)
    # Find parameter vector
-   w = solve(t(X) %*% X) %*% t(X) %*% Y
+   w = solve(t(x_train) %*% x_train) %*% t(x_train) %*% y_train
+   w = as.vector(w)
    # Predicted values
-   pY = X %*% w
+   pY = x_test %*% w
    # Calculate error rate
-   errors = sum((pY - Y)^2)
+   errors = sum((y_test - pY)^2)
    return(list(param=w, pred=pY, err=errors))
  }
 
 data = swiss
 # ============
 #fold_indexes(nrow(data),5)
-best_subset(data,data,3)
+X = data[,2:ncol(data)]
+Y = as.matrix(data$Fertility)
+print(best_subset(X,Y,5))
 # 
 # set.seed(12345)
 # s = sample(1:nrow(data),nrow(data))
